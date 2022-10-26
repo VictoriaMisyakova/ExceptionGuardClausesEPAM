146fd8a9b13135fe2a53b07293889029623305b3
Виктория Мисякова <vmisyakova@gmail.com>
2022-10-26 15:11:10 +0300

    Fix compiler issues

---
+++ ExceptionGuardClauses/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "CA2201:Не порождайте исключения зарезервированных типов", Justification = "<Ожидание>", Scope = "member", Target = "~M:ExceptionGuardClauses.ThrowingExceptions.GetArrayValue(System.Int32[],System.Int32,System.String[])~System.String")]
+[assembly: SuppressMessage("Major Code Smell", "S112:General exceptions should never be thrown", Justification = "<Ожидание>", Scope = "member", Target = "~M:ExceptionGuardClauses.ThrowingExceptions.GetArrayValue(System.Int32[],System.Int32,System.String[])~System.String")]
